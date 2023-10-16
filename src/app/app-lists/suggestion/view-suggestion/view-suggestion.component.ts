6e4144ecbd482de5ccc71a1780a63152cc943d75
ashiq.ansari <ashiq.ansari@novacisdigital.com>
2023-10-16 11:19:26 +0530

    add suggestion form fileds add 1

--- src/app/app-lists/suggestion/view-suggestion/view-suggestion.component.ts
+++ src/app/app-lists/suggestion/view-suggestion/view-suggestion.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-view-suggestion',
@@ -6,9 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-suggestion.component.scss']
 })
 export class ViewSuggestionComponent {
+  addSuggestionForm!: FormGroup;
+  showAddSuggestion: boolean = false;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   createSuggestion() {
-
+    this.showAddSuggestion = true;
+    this.addSuggestionForm = this.formBuilder.group({
+      empId: ['', Validators.required],
+      empName: ['', Validators.required],
+      department: ['', Validators.required],
+      line: ['', Validators.required],
+      zone: ['', Validators.required],
+      title: ['', Validators.required],
+      issue: ['', Validators.required],
+      idea: ['', Validators.required],
+      remarks: ['', Validators.required]
+    })
   }
 
 }
