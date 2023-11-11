857d0b22a1906eaf459cbe1f32f5a6b74d097261
ashiq.ansari <ashiq.ansari@novacisdigital.com>
2023-11-11 12:50:52 +0530

    changes till 11-11 morning

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators  } from '@angular/forms';
 import { Router } from '@angular/router';
 import 'zone.js';
 // import 'zone.js/dist/long-stack-trace-zone.js';
 import { AuthenticationService } from '../services/authentication.service';
+import { CustomValidators } from '../services/password-validator';
 
 @Component({
   selector: 'app-login',
   templateUrl: 'login.component.html',
   styleUrls: ['login.component.scss'],
 })
+  
 export class LoginComponent {
   loginForm!: FormGroup;
   isUserInvalid = false;
@@ -29,7 +31,10 @@ export class LoginComponent {
       empId: ['', Validators.required],
       password: ['', Validators.required],
       confirmPassword: ''
-    });
+    },
+    // { validators: passwordMatchingValidatior }
+      // [CustomValidators.MatchValidator('password', 'confirmPassword')]
+    );
     this.loginForm.get('confirmPassword')?.disable();
 
     
@@ -47,15 +52,11 @@ export class LoginComponent {
         this.loginForm.reset();
         this.isUserInvalid = false;
         // this.router.navigate(['user/viewuser']);
-      } else if (res?.Status == 0) {
+      } else if (res?.Status == 2) {
         this.loginForm.get('empId')?.disable();
-        // this.loginForm.markAsUntouched();
-        // this.loginForm.get('password')?.patchValue('');
         this.loginForm.get('password')?.reset();
-        // this.loginForm.get('password')?.updateValueAndValidity();
-        // this.loginForm.get('password')?.markAsUntouched();
         this.loginForm.get('confirmPassword')?.enable();
-        this.loginForm.get('confirmPassword')?.setValidators(Validators.required);
+        this.loginForm.get('confirmPassword')?.setValidators(Validators.required);        
       } else {
         this.isUserInvalid = true;
         this.authService.isLoggedIn = false;
@@ -65,21 +66,62 @@ export class LoginComponent {
     });
   }
 
+  updatePassword() {
+    this.showLoader = true;
+    let userObj = {
+      EmpID: this.loginForm.get('empId')?.value,
+      password: this.loginForm.get('password')?.value,
+    };
+    this.authService.login(userObj).subscribe((res: any) => {
+      if (res?.Status == 1) {
+        this.authService.isLoggedIn = true;
+        this.loginForm.reset();
+        this.isUserInvalid = false;
+        this.router.navigate(['suggestion/viewSuggestion']);
+      }
+     })
+  }
+
   togglePasswordVisibility() {
     this.viewPassword = !this.viewPassword;
   }
 
-  comparePassword() {
-    if (this.loginForm.get('password')?.value != this.loginForm.get('confirmPassword')?.value) {
-      this.notMatch = true;
-      console.log(this.notMatch)
-    } else {
-      this.notMatch = false;
-      console.log(this.notMatch)
-    }
+  get passwordMatchError() {
+    return
+    // return (this.loginForm.get('confirmPassword')?.touched && this.loginForm.get('password')?.value != this.loginForm.get('confirmPassword')?.value);
+    // console.log(this.loginForm)
+    // return (
+    //   this.loginForm.getError('mismatch') &&
+    //   this.loginForm.get('confirmPassword')?.touched
+    // );
   }
 
   navigateToViewSuggestion() {
     this.router.navigate(['suggestion/viewSuggestion']);
   }
+
+  // All is this method
+onPasswordChange() {
+  if (this.loginForm.controls['confirmPassword'].status == 'DISABLED' || this.confirmPassword.value == this.password.value) {
+    this.confirmPassword.setErrors(null);
+  } else {
+    this.confirmPassword.setErrors({ mismatch: true });
+  }
 }
+
+// getting the form control elements
+get password(): AbstractControl {
+  return this.loginForm.controls['password'];
+}
+
+get confirmPassword(): AbstractControl {
+  return this.loginForm.controls['confirmPassword'];
+}
+}
+
+export const passwordMatchingValidatior: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const password = control.get('password');
+  const confirmPassword = control.get('confirmPassword');
+
+  return password?.value === confirmPassword?.value ? null : { notmatched: true };
+};
